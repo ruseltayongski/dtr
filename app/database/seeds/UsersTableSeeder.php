86f9f9da9bae58f5436ff2bfc6a64c14fd85ca8f
resxustrya <rexustraya@gmail.com>
2017-04-06 17:06:02 +0800

    Push updat

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new Users();
+        $user->username = "admin";
+        $user->fname = 'Lourence Rex';
+        $user->lname = 'Traya';
+        $user->mname = 'B.';
+        $user->password = Hash::make("admin");
+        $user->usertype = 1;
+        $user->save();
+    }
+}
--- app/models/Users.php
+++ app/models/Users.php
@@ -1,6 +1,11 @@
 <?php
 
-
+/**
+ * Created by PhpStorm.
+ * User: Lourence
+ * Date: 4/6/2017
+ * Time: 5:04 PM
+ */
 class Users extends Eloquent
 {
     protected $table = 'users';
