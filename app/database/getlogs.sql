45dfe9b0d739746e0ee31c652b5ccd8317563dae
Lourence Rex Traya <=>
2017-06-20 11:05:39 +0800

    Push update

--- app/database/getlogs.sql
+++ app/database/getlogs.sql
@@ -1,23 +1,43 @@
-call dohdtr.GETLOGS('08:00:00', '12:00:00', '13:00:00', '18:00:00', '0546', '2017-06-01', '2017-06-31');
+USE `dohdtr`;
+DROP procedure IF EXISTS `GETLOGS`;
+
+DELIMITER $$
+USE `dohdtr`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GETLOGS`(IN AMIN VARCHAR(10), IN AMOUT VARCHAR(10), IN PMIN VARCHAR(10), IN PMOUT VARCHAR(10), IN ID VARCHAR(10), IN DATE_FROM VARCHAR(10), IN DATE_TO VARCHAR(10))
+    READS SQL DATA
+    DETERMINISTIC
+BEGIN
 
 
-IF((SELECT a.time FROM dtr_file a WHERE  userid = d.userid and datein = d.datein AND (a.time > (SELECT time from dtr_file where userid = d.userid and datein = d.datein and time < AMOUT ORDER BY time asc limit 1 )) AND a.time < PMOUT ORDER BY a.time ASC LIMIT 1),
-		#IF TRUE
-		(SELECT t2.time FROM dtr_file t2 WHERE  userid = d.userid and datein = d.datein AND (t2.time > (SELECT time from dtr_file where userid = d.userid and datein = d.datein and time < AMOUT ORDER BY time asc limit 1 )) AND t2.time < PMOUT ORDER BY t2.time ASC LIMIT 1),
-		#IF FALSE
-		()
-	 )
-     
-     
-     
-	(SELECT CONCAT(t1.time,'_', t1.edited) FROM dtr_file t1 WHERE userid = d.userid and datein = d.datein and t1.time < AMOUT ORDER BY time ASC LIMIT 1) as am_in,
+SELECT DISTINCT e.userid, datein,holiday,remark, 
 
-	(SELECT CONCAT(t2.time,'_',t2.edited)  FROM dtr_file t2 WHERE  userid = d.userid and datein = d.datein AND (t2.time > (SELECT time from dtr_file where userid = d.userid and datein = d.datein and time < AMOUT ORDER BY time asc limit 1 )) AND t2.time < PMOUT ORDER BY t2.time ASC LIMIT 1) as am_out,
-	 
-	 
-	(SELECT CONCAT(t3.time,'_',t3.edited) FROM dtr_file t3 WHERE userid = d.userid and datein = d.datein AND t3.time > IF(TRUE,'','')
+	(SELECT  CONCAT(t1.time,'_',t1.edited) FROM dtr_file t1 WHERE userid = d.userid and datein = d.datein and t1.time < AMOUT AND t1.event = 'IN' ORDER BY time ASC  LIMIT 1) as am_in,
+    
+	(SELECT  CONCAT(t2.time,'_',t2.edited) FROM dtr_file t2 WHERE userid = d.userid and datein = d.datein and (SELECT  CONCAT(t1.time,'_',t1.edited) FROM dtr_file t1 WHERE userid = d.userid and datein = d.datein and t1.time < AMOUT AND t1.event = 'IN' ORDER BY time ASC  LIMIT 1) and  t2.time < PMIN  AND t2.event = 'OUT' AND t2.time > AMIN ORDER BY t2.time DESC LIMIT 1 ) as am_out,
+    
+	(SELECT  CONCAT(t3.time,'_',t3.edited) FROM dtr_file t3 WHERE userid = d.userid AND datein = d.datein and t3.time > AMOUT and t3.time < PMOUT AND t3.event = 'IN' ORDER BY t3.time ASC LIMIT 1) as pm_in,
+    
+	(SELECT  CONCAT(t4.time,'_',t4.edited) FROM dtr_file t4 WHERE userid = d.userid AND datein = d.datein and t4.time > PMIN AND t4.event = 'OUT'
+    
+		AND t4.time > IF((SELECT  CONCAT(t3.time,'_',t3.edited) FROM dtr_file t3 WHERE userid = d.userid AND datein = d.datein and t3.time > AMOUT and t3.time < PMOUT AND t3.event = 'IN' ORDER BY t3.time ASC LIMIT 1),
+        (SELECT  CONCAT(t3.time,'_',t3.edited) FROM dtr_file t3 WHERE userid = d.userid AND datein = d.datein and t3.time > AMOUT and t3.time < PMOUT AND t3.event = 'IN' ORDER BY t3.time ASC LIMIT 1),
+        (0))
+    
+    ORDER BY time DESC LIMIT 1) as pm_out
 
-	
-	 ORDER BY t3.time ASC LIMIT 1 ) as pm_in,
-	
-     (SELECT  CONCAT(MAX(t4.time),'_',t4.edited) FROM dtr_file t4 WHERE userid = d.userid AND datein = d.datein and t4.time > PMIN ORDER BY time DESC LIMIT 1) as pm_out
+    
+FROM dtr_file d LEFT JOIN users e
+	ON d.userid = e.userid and datein = d.datein 
+    or (datein between DATE_FROM AND DATE_TO and holiday = '001') 
+    or (datein between DATE_FROM AND DATE_TO and holiday = '002' and d.userid = e.userid)
+    or (datein between DATE_FROM AND DATE_TO and holiday = '003' and d.userid = e.userid)
+WHERE d.datein BETWEEN DATE_FROM AND DATE_TO
+	  AND e.userid = ID 
+	group by d.datein
+ORDER BY datein ASC;
+
+
+END$$
+
+DELIMITER ;
+
