0e7b76a14e26e5d3fc2d3fc25ced56f86fdd34c2
Lourence Rex Traya <=>
2017-06-22 11:27:49 +0800

    Push update

--- app/database/getlogs.sql
+++ app/database/getlogs.sql
@@ -8,16 +8,16 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `GETLOGS`(IN AMIN VARCHAR(10), IN AM
     DETERMINISTIC
 BEGIN
 
-DECLARE A VARCHAR(20);
+
 
 
 SELECT DISTINCT e.userid, datein,holiday,remark, 
 
 	(SELECT  CONCAT(t1.time,'_',t1.edited) FROM dtr_file t1 WHERE userid = d.userid and datein = d.datein and t1.time < AMOUT AND t1.event = 'IN' ORDER BY time ASC  LIMIT 1) as am_in,
     
-	(SELECT  CONCAT(t2.time,'_',t2.edited) FROM dtr_file t2 WHERE userid = d.userid and datein = d.datein and (SELECT  CONCAT(t1.time,'_',t1.edited) FROM dtr_file t1 WHERE userid = d.userid and datein = d.datein and t1.time < AMOUT AND t1.event = 'IN' ORDER BY time ASC  LIMIT 1) and  t2.time < PMIN  AND t2.event = 'OUT' AND t2.time > AMIN ORDER BY t2.time DESC LIMIT 1 ) as am_out,
+	(SELECT  CONCAT(t2.time,'_',t2.edited) FROM dtr_file t2 WHERE userid = d.userid and datein = d.datein and (SELECT  CONCAT(t1.time,'_',t1.edited) FROM dtr_file t1 WHERE userid = d.userid and datein = d.datein and t1.time < AMOUT AND t1.event = 'IN' ORDER BY time ASC  LIMIT 1) and  t2.time < PMIN AND t2.event = 'OUT' AND t2.time > AMIN ORDER BY t2.time DESC LIMIT 1 ) as am_out,
     
-	(SELECT  CONCAT(t3.time,'_',t3.edited) FROM dtr_file t3 WHERE userid = d.userid AND datein = d.datein and t3.time < '14:00:00' and t3.time > AMOUT and t3.time < PMOUT AND t3.event = 'IN' ORDER BY t3.time DESC LIMIT 1) as pm_in,
+	(SELECT  CONCAT(t3.time,'_',t3.edited) FROM dtr_file t3 WHERE userid = d.userid AND datein = d.datein and t3.time > AMOUT and t3.time < PMOUT AND t3.event = 'IN' ORDER BY t3.time ASC LIMIT 1) as pm_in,
     
 	(SELECT  CONCAT(t4.time,'_',t4.edited) FROM dtr_file t4 WHERE userid = d.userid AND datein = d.datein and t4.time > PMIN AND t4.event = 'OUT'
     
@@ -30,11 +30,9 @@ SELECT DISTINCT e.userid, datein,holiday,remark,
     
 FROM dtr_file d LEFT JOIN users e
 	ON d.userid = e.userid and datein = d.datein 
-    or (datein between DATE_FROM AND DATE_TO and holiday = '001') /*HOLIDAY*/
-    or (datein between DATE_FROM AND DATE_TO and holiday = '002' and d.userid = e.userid) /*CTO*/
-    or (datein between DATE_FROM AND DATE_TO and holiday = '003' and d.userid = e.userid) /*SO*/
-    or (datein between DATE_FROM AND DATE_TO and holiday = '004' and d.userid = e.userid) /*LEAVE*/
-    or (datein between DATE_FROM AND DATE_TO and holiday = '005' and d.userid = e.userid) /*ABSENT*/
+    or (datein between DATE_FROM AND DATE_TO and holiday = '001') 
+    or (datein between DATE_FROM AND DATE_TO and holiday = '002' and d.userid = e.userid)
+    or (datein between DATE_FROM AND DATE_TO and holiday = '003' and d.userid = e.userid)
 WHERE d.datein BETWEEN DATE_FROM AND DATE_TO
 	  AND e.userid = ID 
 	group by d.datein
