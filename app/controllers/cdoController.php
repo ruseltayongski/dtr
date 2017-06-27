cf8cb7970f465baa2afdc7107452259bebb4b8a3
Rusel T. Tayong <rtayong@doh7.net>
2017-06-27 08:56:15 +0800

    'PushUpdates'

--- app/controllers/cdoController.php
+++ app/controllers/cdoController.php
@@ -8,6 +8,7 @@ class cdoController extends BaseController
     }
 
     public function cdo_list(){
+        Session::put('page',null);
         Session::put('keyword',Input::get('keyword'));
         $keyword = Session::get('keyword');
 
@@ -16,7 +17,7 @@ class cdoController extends BaseController
                 ->where(function($q) use ($keyword){
                     $q->where("route_no","like","%$keyword%")
                         ->orWhere("subject","like","%$keyword%");
-            })
+                })
                 ->orderBy('id','desc')
                 ->paginate(2);
             $cdo[1] = cdo::where('approved_status',1)
@@ -24,6 +25,12 @@ class cdoController extends BaseController
                     $q->where("route_no","like","%$keyword%")
                         ->orWhere("subject","like","%$keyword%");
                 })
+                ->orderBy('id','desc')
+                ->paginate(2);
+            $cdo[2] = cdo::where(function($q) use ($keyword){
+                $q->where("route_no","like","%$keyword%")
+                    ->orWhere("subject","like","%$keyword%");
+            })
                 ->orderBy('id','desc')
                 ->paginate(2);
             $type = "pending";
@@ -34,15 +41,18 @@ class cdoController extends BaseController
                         ->orWhere("subject","like","%$keyword%");
                 })
                 ->orderBy('id','desc')
-                ->paginate(10);
+                ->paginate(2);
             $type = "list";
         }
 
         if (Request::ajax()) {
+            Session::put('page',Input::get('page'));
             if(Input::get('type') == 'approve')
                 $view = 'cdo.cdo_approve';
             elseif(Input::get('type') == 'disapprove')
                 $view = 'cdo.cdo_disapprove';
+            elseif(Input::get('type') == 'all')
+                $view = 'cdo.cdo_all';
 
             return Response::json(View::make($view, ["cdo" => $cdo,"type" => $type])->render());
         }
@@ -136,113 +146,143 @@ class cdoController extends BaseController
         return Redirect::to('form/cdo_list');
     }
 
-    public function cdo_updatev1(){
-        $route_no = Session::get('route_no');
-        $prepared_date = date('Y-m-d',strtotime(Input::get('prepared_date'))).' '.date('H:i:s');
-        $info = cdo::where('route_no',$route_no)->first();
-
-        $str = Input::get('inclusive_dates');
-        $temp1 = explode('-',$str);
-        $temp2 = array_slice($temp1, 0, 1);
-        $tmp = implode(',', $temp2);
-        $start_date = date('Y-m-d',strtotime($tmp));
-
-        $temp3 = array_slice($temp1, 1, 1);
-        $tmp = implode(',', $temp3);
-        $enddate = date_create(date('Y-m-d',strtotime($tmp)));
-        date_add($enddate, date_interval_create_from_date_string('1days'));
-        $end_date = date_format($enddate, 'Y-m-d');
-        $subject = Input::get('subject');
-        $working_days = floor(strtotime($end_date) / (60 * 60 * 24)) - floor(strtotime($start_date) / (60 * 60 * 24)) - 1;
-
-        if(Auth::user()->usertype){
-            $beginning_balance = Input::get('beginning_balance');
-            $less_applied = Input::get('less_applied');
-            $remaining_balance = Input::get('remaining_balance');
-        } else{
-            $beginning_balance = $info->beginning_balance;
-            $less_applied = $info->less_applied_for;
-            $remaining_balance = $info->remaining_balance;
-        }
-        if(Auth::user()->usertype and Input::get('approval')) {
-
-            //delete dtr file
-            DtrDetails::where('holiday','=', '002')
-                ->whereBetween('datein',array($info->start,$info->end))
-                ->delete();
-
-            $approved_status = 1;
-            $dtr_enddate  = date('Y-m-d',(strtotime ( '-1 day' , strtotime ($end_date))));
-
-            $f = new DateTime($start_date.' '. '00:00:00');
-            $t = new DateTime($dtr_enddate.' '. '00:00:00');
-
-            $interval = $f->diff($t);
-
-            $datein = '';
-            $f_from = explode('-',$start_date);
-            $startday = $f_from[2];
-            $j = 0;
-            while($j <= $interval->days) {
-
-                $time = array('08:00:00','12:00:00','13:00:00','18:00:00');
-                $datein = $f_from[0].'-'.$f_from[1] .'-'. $startday;
-
-                for($i = 0; $i < count($time); $i++):
-                    $details = new DtrDetails();
-                    $details->userid = pdoController::user_search1($info->prepared_name)['username'];
-                    $details->datein = $datein;
-                    $details->time = $time[$i];
-                    $details->event = 'IN';
-                    $details->remark = 'CTO';
-                    $details->edited = '1';
-                    $details->holiday = '002';
-
-                    $details->save();
-                endfor;
-
-                $startday = $startday + 1;
-                $j++;
+    public function cdo_updatev1($id = null,$type = null){
+        if($id){
+            $cdo = cdo::where('id',$id)->first();
+            if($cdo->approved_status){
+                $cdo->approved_status = 0;
+            } else {
+                $cdo->approved_status = 1;
             }
-        }
-        elseif(Input::get('disapproval')) {
-            $approved_status = 0;
-            //delete dtr file
-            DtrDetails::where('holiday','=', '002')
-                ->whereBetween('datein',array($info->start,$info->end))->delete();
+            $cdo->save();
+            $keyword = '';
+            $cdo[2] = cdo::where(function($q) use ($keyword){
+                $q->where("route_no","like","%$keyword%")
+                    ->orWhere("subject","like","%$keyword%");
+            })
+                ->orderBy('id','desc')
+                ->paginate(2);
+            if(Request::ajax()) {
+                if($type == 'approve')
+                    $view = 'cdo.cdo_approve';
+                elseif($type == 'disapprove')
+                    $view = 'cdo.cdo_disapprove';
+                elseif($type == 'all')
+                    $view = 'cdo.cdo_all';
+
+                return Response::json(View::make($view, ["cdo" => $cdo,"type" => $type])->render());
+            } else {
+                return 'walley';
+            }
+            //return Response::json(View::make('cdo.cdo_all', ["cdo" => $cdo,"type" => $type])->render());
         } else {
-            $approved_status = 0;
+            $route_no = Session::get('route_no');
+            $prepared_date = date('Y-m-d',strtotime(Input::get('prepared_date'))).' '.date('H:i:s');
+            $info = cdo::where('route_no',$route_no)->first();
+
+            $str = Input::get('inclusive_dates');
+            $temp1 = explode('-',$str);
+            $temp2 = array_slice($temp1, 0, 1);
+            $tmp = implode(',', $temp2);
+            $start_date = date('Y-m-d',strtotime($tmp));
+
+            $temp3 = array_slice($temp1, 1, 1);
+            $tmp = implode(',', $temp3);
+            $enddate = date_create(date('Y-m-d',strtotime($tmp)));
+            date_add($enddate, date_interval_create_from_date_string('1days'));
+            $end_date = date_format($enddate, 'Y-m-d');
+            $subject = Input::get('subject');
+            $working_days = floor(strtotime($end_date) / (60 * 60 * 24)) - floor(strtotime($start_date) / (60 * 60 * 24)) - 1;
+
+            if(Auth::user()->usertype){
+                $beginning_balance = Input::get('beginning_balance');
+                $less_applied = Input::get('less_applied');
+                $remaining_balance = Input::get('remaining_balance');
+            } else{
+                $beginning_balance = $info->beginning_balance;
+                $less_applied = $info->less_applied_for;
+                $remaining_balance = $info->remaining_balance;
+            }
+            if(Auth::user()->usertype and Input::get('approval')) {
+
+                //delete dtr file
+                DtrDetails::where('holiday','=', '002')
+                    ->whereBetween('datein',array($info->start,$info->end))
+                    ->delete();
+
+                $approved_status = 1;
+                $dtr_enddate  = date('Y-m-d',(strtotime ( '-1 day' , strtotime ($end_date))));
+
+                $f = new DateTime($start_date.' '. '00:00:00');
+                $t = new DateTime($dtr_enddate.' '. '00:00:00');
+
+                $interval = $f->diff($t);
+
+                $datein = '';
+                $f_from = explode('-',$start_date);
+                $startday = $f_from[2];
+                $j = 0;
+                while($j <= $interval->days) {
+
+                    $time = array('08:00:00','12:00:00','13:00:00','18:00:00');
+                    $datein = $f_from[0].'-'.$f_from[1] .'-'. $startday;
+
+                    for($i = 0; $i < count($time); $i++):
+                        $details = new DtrDetails();
+                        $details->userid = pdoController::user_search1($info->prepared_name)['username'];
+                        $details->datein = $datein;
+                        $details->time = $time[$i];
+                        $details->event = 'IN';
+                        $details->remark = 'CTO';
+                        $details->edited = '1';
+                        $details->holiday = '002';
+
+                        $details->save();
+                    endfor;
+
+                    $startday = $startday + 1;
+                    $j++;
+                }
+            }
+            elseif(Input::get('disapproval')) {
+                $approved_status = 0;
+                //delete dtr file
+                DtrDetails::where('holiday','=', '002')
+                    ->whereBetween('datein',array($info->start,$info->end))->delete();
+            } else {
+                $approved_status = 0;
+            }
+
+            //UPDATE CDO
+            cdo::where("route_no",$route_no)->update([
+                "subject" => $subject,
+                "prepared_date" => $prepared_date,
+                "working_days" => $working_days,
+                "start" => $start_date,
+                "end" => $end_date,
+                "beginning_balance" => $beginning_balance,
+                "less_applied_for" => $less_applied,
+                "remaining_balance" => $remaining_balance,
+                "immediate_supervisor" => Input::get('immediate_supervisor'),
+                "division_chief" => Input::get('division_chief'),
+                "approved_status" => $approved_status
+            ]);
+
+            //UPDATE TRACKING MASTER
+            pdoController::update_tracking_master($prepared_date,$subject,$route_no);
+
+            //UPDATE TRACKING DETAILS
+            pdoController::update_tracking_details($subject,$route_no);
+
+            //ADD SYSTEM LOGS
+            $user_id = $info->prepared_name;
+            $name = Auth::user()->fname.' '.Auth::user()->mname.' '.Auth::user()->lname;
+            $activity = 'Updated';
+            pdoController::insert_system_logs($user_id,$name,$activity,$route_no);
+
+            Session::put('updated',true);
+            return Redirect::to('form/cdo_list');
         }
-
-        //UPDATE CDO
-        cdo::where("route_no",$route_no)->update([
-            "subject" => $subject,
-            "prepared_date" => $prepared_date,
-            "working_days" => $working_days,
-            "start" => $start_date,
-            "end" => $end_date,
-            "beginning_balance" => $beginning_balance,
-            "less_applied_for" => $less_applied,
-            "remaining_balance" => $remaining_balance,
-            "immediate_supervisor" => Input::get('immediate_supervisor'),
-            "division_chief" => Input::get('division_chief'),
-            "approved_status" => $approved_status
-        ]);
-
-        //UPDATE TRACKING MASTER
-        pdoController::update_tracking_master($prepared_date,$subject,$route_no);
-
-        //UPDATE TRACKING DETAILS
-        pdoController::update_tracking_details($subject,$route_no);
-
-        //ADD SYSTEM LOGS
-        $user_id = $info->prepared_name;
-        $name = Auth::user()->fname.' '.Auth::user()->mname.' '.Auth::user()->lname;
-        $activity = 'Updated';
-        pdoController::insert_system_logs($user_id,$name,$activity,$route_no);
-
-        Session::put('updated',true);
-        return Redirect::to('form/cdo_list');
     }
 
     public function cdo_delete(){
