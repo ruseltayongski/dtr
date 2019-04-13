091c3b958496a3d18f6d43e17696d9d1336580a4
Rusel T. Tayong <ruseltayong@gmail.com>
2019-04-12 22:40:53 -0700

    updates

--- app/controllers/GitController.php
+++ app/controllers/GitController.php
@@ -1,13 +1,16 @@
 <?php
 
 class GitController extends BaseController{
-    public function git_pull(){
-        return json_encode(exec("git pull origin master"));
-    }
     public function git_add(){
         return json_encode(exec("git add ."));
     }
     public function git_commit(){
-        return json_encode(exec("git commit -m 'PullUpdates'"));
+        return json_encode(exec("git commit -m 'UpdatesRepository'"));
+    }
+    public function git_pull(){
+        return json_encode(exec("git pull origin master"));
+    }
+    public function git_push(){
+        return json_encode(exec("git push origin master"));
     }
 }
