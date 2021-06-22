2752700864eba2ef4ebfeb6cc1af293a5308b951
ruseltayongski <ruseltayong@gmail.com>
2021-06-22 14:47:02 +0800

    PUshUPdates

--- app/controllers/MobileControllerV2.php
+++ app/controllers/MobileControllerV2.php
@@ -212,49 +212,55 @@ class MobileControllerV2 extends BaseController
 
     public function add_logs()
     {
-        try{
-            $json_object = json_decode(Input::get('data'), true);
-            foreach ($json_object['logs'] as $value) {
-                $userid = $value['userid'];
-                $time = $value['time'];
-                $event = $value['event'];
-                $date = $value['date'];
-                $lat = $value['latitude'];
-                $long = $value['longitude'];
-                $remark = $value['remark'];
-                $edited = $value['edited'];
-                $base = $value['image'];
-                $posted_filename = explode('_',$value['filename'])[1];
-                $picture_type = explode('_',$value['filename'])[0];
+        $check_dmo = UserRoles::select('users_roles.id','users_claims.claim_type','users_claims.claim_value','users_claims.id as claims_id')
+            ->where('users_roles.userid','=',Auth::user()->userid)
+            ->where('users_roles.claims_id','=','4')
+            ->LeftJoin('users_claims','users_claims.id','=','users_roles.claims_id')
+            ->first();
+        if($check_dmo){
+            try{
+                $json_object = json_decode(Input::get('data'), true);
+                foreach ($json_object['logs'] as $value) {
+                    $userid = $value['userid'];
+                    $time = $value['time'];
+                    $event = $value['event'];
+                    $date = $value['date'];
+                    $lat = $value['latitude'];
+                    $long = $value['longitude'];
+                    $remark = $value['remark'];
+                    $edited = $value['edited'];
+                    $base = $value['image'];
+                    $posted_filename = explode('_',$value['filename'])[1];
+                    $picture_type = explode('_',$value['filename'])[0];
 
-                $binary = base64_decode($base);
-                if (!file_exists(public_path().'/logs_imageV2'.'/'.$userid)) {
-                    mkdir(public_path().'/logs_imageV2'.'/'.$userid, 0777, true);
-                    mkdir(public_path().'/logs_imageV2'.'/'.$userid.'/screenshot', 0777, true);
-                    mkdir(public_path().'/logs_imageV2'.'/'.$userid.'/timelog', 0777, true);
+                    $binary = base64_decode($base);
+                    if (!file_exists(public_path().'/logs_imageV2'.'/'.$userid)) {
+                        mkdir(public_path().'/logs_imageV2'.'/'.$userid, 0777, true);
+                        mkdir(public_path().'/logs_imageV2'.'/'.$userid.'/screenshot', 0777, true);
+                        mkdir(public_path().'/logs_imageV2'.'/'.$userid.'/timelog', 0777, true);
+                    }
+                    file_put_contents(public_path().'/logs_imageV2'.'/'.$userid.'/'.$picture_type.'/'.$posted_filename, $binary);
+
+                    $pdo = DB::connection()->getPdo();
+
+                    $query1 = "INSERT IGNORE INTO dtr_file(userid, datein, time, event,remark, created_at, updated_at,log_image,edited,latitude,longitude) VALUES";
+                    $query1 .= "('" . $userid . "','" . $date . "','" . $time . "','" . $event . "','$remark',NOW(),NOW(),'$posted_filename','$edited','$lat','$long')";
+
+                    $st = $pdo->prepare($query1);
+                    $st->execute();
                 }
-                file_put_contents(public_path().'/logs_imageV2'.'/'.$userid.'/'.$picture_type.'/'.$posted_filename, $binary);
 
-                $pdo = DB::connection()->getPdo();
-
-                $query1 = "INSERT IGNORE INTO dtr_file(userid, datein, time, event,remark, created_at, updated_at,log_image,edited,latitude,longitude) VALUES";
-                $query1 .= "('" . $userid . "','" . $date . "','" . $time . "','" . $event . "','$remark',NOW(),NOW(),'$posted_filename','$edited','$lat','$long')";
-
-                $st = $pdo->prepare($query1);
-                $st->execute();
+                return [
+                    "code" => 200,
+                    "response" => "Successfully uploaded"
+                ];
+            } catch (Exception $e){
+                return [
+                    "code" => 0,
+                    "response" => $e->getMessage()
+                ];
             }
-
-            return [
-                "code" => 200,
-                "response" => "Successfully uploaded"
-            ];
-        } catch (Exception $e){
-            return [
-                "code" => 0,
-                "response" => $e->getMessage()
-            ];
         }
-
 
     }
 
