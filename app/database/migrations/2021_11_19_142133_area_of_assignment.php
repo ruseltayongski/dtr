3de22ffa59ea5c50a5dfb287c23f6a43026f01fd
ChristineAnne <christineannecatubig@gmail.com>
2021-11-19 14:32:10 +0800

    added migration for area of assignment

---
+++ app/database/migrations/2021_11_19_142133_area_of_assignment.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AreaOfAssignment extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if(Schema::hasTable('area_of_assignment')){
+            return true;
+        }
+        Schema::create('area_of_assignment', function($table) {
+        	$table->increments('id');
+			$table->string('name',255);
+			$table->string('latitude',255);
+			$table->string('longitude',255);
+			$table->integer('radius')
+			$table->timestamps();
+			$table->softDeletes();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('area_of_assignment');
+	}
+
+}
