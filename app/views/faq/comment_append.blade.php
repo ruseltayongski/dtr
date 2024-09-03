ac866f2a917707e09b420dd774d2c74b6f4eeaa4
Angelica1999 <maangelica.cadayday@gmail.com>
2024-09-03 11:27:50 +0800

    fix queries ui

--- app/views/faq/comment_append.blade.php
+++ app/views/faq/comment_append.blade.php
@@ -1,19 +1,30 @@
 <div class="reply_append{{ $comment_id }}">
     <div class="item" id="{{ $comment_id }}">
         <?php $commentUser = InformationPersonal::where('userid',Auth::user()->userid)->first() ?>
-        <img src="{{ isset($commentUser->picture) ? str_replace('dtr','pis',asset('')).'public/upload_picture/picture/'.$commentUser->picture : str_replace('dtr','pis',asset('')).'public/upload_picture/picture/uknown.png' }}" alt="user image" class="online">
-        <p class="message">
-            <span href="#" class="name text-blue" style="display: inline;">
-                {{ Auth::user()->lname.', '.Auth::user()->fname }}
-            </span>{{ $description }} <br>
-            <a data-toggle="collapse" class="text-blue" href="#collapse{{ $comment_id }}" aria-expanded="false" aria-controls="collapseExample" style="font-size: 8pt"> Reply</a>
-        </p>
+        <div class="row">
+            <div class="col-md-1">
+                <div class="item">
+                    <img src="{{ isset($commentUser->picture) ? 'https://pis.cvchd7.com/pis/public/upload_picture/picture/'.$commentUser->picture : 'https://pis.cvchd7.com/pis/public/upload_picture/picture/uknown.png' }}" alt="user image" class="online">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="message">
+                    <span href="#" class="name text-blue" style="display: inline;">
+                        {{ strtoupper(Auth::user()->lname.', '.Auth::user()->fname) }}
+                    </span>
+                    <small class="text-success"><br>
+                        <p>{{ trim(strip_tags(nl2br($description))) }}</p>
+                    </small>
+                    <a data-toggle="collapse" class="text-blue" href="#collapse{{ $comment_id }}" aria-expanded="false" aria-controls="collapseExample" style="font-size: 8pt"> Reply</a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <div class="collapse" id="collapse{{ $comment_id }}">
     <div class="box-footer" style="margin-left:5%;">
         <form action="#" method="post" class="submit_reply_append{{ $comment_id }}" autocomplete="off">
-            <img class="img-responsive img-circle img-sm" src="{{ isset($commentUser->picture) ? str_replace('dtr','pis',asset('')).'public/upload_picture/picture/'.$commentUser->picture : str_replace('dtr','pis',asset('')).'public/upload_picture/picture/uknown.png' }}" alt="Alt Text">
+            <img class="img-responsive img-circle img-sm" src="{{ isset($commentUser->picture) ? 'https://pis.cvchd7.com/pis/public/upload_picture/picture/'.$commentUser->picture :'https://pis.cvchd7.com/pis/public/upload_picture/picture/uknown.png' }}" alt="Alt Text">
             <!-- .img-push is used to add margin to elements next to floating images -->
             <div class="img-push">
                 <input type="text" class="form-control input-sm" id="text_reply{{ $comment_id }}" placeholder="Press enter to reply">
--- app/views/faq/reply_append.blade.php
+++ app/views/faq/reply_append.blade.php
@@ -1,6 +1,7 @@
-<div class="item" id="reply{{ $count }}" style="margin-left:5%;">
+<div class="item" id="reply{{ $count }}" style="margin-left:8%;margin-top: 1%">
     <?php $replyUser = InformationPersonal::where('userid','=',Auth::user()->userid)->first(); ?>
-    <img src="{{ isset($replyUser->picture) ? str_replace('dtr','pis',asset('')).'public/upload_picture/picture/'.InformationPersonal::where('userid','=',$replyUser->userid)->first()->picture : str_replace('dtr','pis',asset('')).'public/upload_picture/picture/uknown.png' }}" alt="user image" class="offline">
+    <img src="{{ isset($replyUser->picture) ? 'https://pis.cvchd7.com/pis/public/upload_picture/picture/'.InformationPersonal::where('userid','=',$replyUser->userid)->first()->picture : 'https://pis.cvchd7.com/pis/public/upload_picture/picture/uknown.png' }}" alt="user image" class="offline">
+
     <p class="message">
             <span href="#" class="name text-blue">
                 <!-- <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 2:15</small> -->
