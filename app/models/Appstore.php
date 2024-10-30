94ce666745294cdfee938d87edadf552afeae58b
Angelica1999 <maangelica.cadayday@gmail.com>
2024-10-30 11:22:25 +0800

    add appstore

---
+++ app/models/Appstore.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Keith
+ * Date: 10/30/2024
+ * Time: 11:16 AM
+ */
+
+class Appstore  extends Eloquent
+{
+    protected $table= 'appstore';
+    protected $primaryKey = 'id';
+}
