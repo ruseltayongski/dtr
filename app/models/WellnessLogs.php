7e7901917538ef22addd21b6d87aa35a92b33ab6
Florsha <floramayjoym@gmail.com>
2025-07-26 15:19:50 +0800

    updatesss

--- app/models/WellnessLogs.php
+++ app/models/WellnessLogs.php
@@ -5,6 +5,10 @@ class WellnessLogs extends Eloquent {
     protected $table = 'wellness_logs';
     protected $primaryKey = 'id';
 
+    protected $fillable = [
+        'remarks'
+    ];
+
     public function wellness()
     {
         return $this->belongsTo('Wellness');
